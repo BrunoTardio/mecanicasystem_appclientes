b2e92d56b9ee90c6e8da4dad7dc11d54a8d23210
Bruno Tardio <brunotardiosi@gmail.com>
2020-03-31 16:14:41 -0300

    POC testando gravacao ao FIRESTORE

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    Firestore db = Firestore.instance;
+    db.collection("usuarios")
+        .document("001")
+        .setData({
+      "nome": "Bruno",
+      "idade":"30"
+    });
+
+
     return Container();
   }
 }
+
