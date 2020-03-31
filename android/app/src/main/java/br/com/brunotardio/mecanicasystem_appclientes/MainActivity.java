d3da31269d1ed105e4647281f1518e645d96c218
Bruno Tardio <brunotardiosi@gmail.com>
2020-03-31 13:43:57 -0300

    Configuracao do projeto com alteracao da chave key.jks

---
+++ android/app/src/main/java/br/com/brunotardio/mecanicasystem_appclientes/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.brunotardio.mecanicasystem_appclientes;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
