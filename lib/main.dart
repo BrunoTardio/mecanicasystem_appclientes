7fecb11a47cb8170579ae95d91ca5198fdddeac4
Bruno Tardio <brunotardiosi@gmail.com>
2020-04-20 15:45:52 -0300

    criacao da tela Home

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,13 @@
-
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'telas/home.dart';
 
-void main() async{
-
-    
-      runApp(MyApp());
-
-      FirebaseAuth auth = FirebaseAuth.instance;
-
-      String email = "brunotardiosi@gmail.com";
-      String senha = "123456";
-
-      auth.createUserWithEmailAndPassword(email: email, password: senha)
-          .then((firebaseUser){
-        print("novo usuario : email "+ firebaseUser.user.email);
-      }).catchError((erro){
-        print(" AQUI erro "+ erro.toString());
-      });
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(
+      primaryColor: Colors.grey[50],
+    ),
+    title: "msCliente",
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
-
-class MyApp extends StatelessWidget  {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
