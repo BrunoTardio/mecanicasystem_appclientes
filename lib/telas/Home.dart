637e29392aa99fb9c3dc6b29af3a75779819c472
Bruno Tardio <brunotardiosi@gmail.com>
2020-04-20 16:45:16 -0300

    Tela de login botoes

--- lib/telas/Home.dart
+++ lib/telas/Home.dart
@@ -9,11 +9,33 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Word'),
+      appBar: AppBar(
+        title: Text("MecanicaSytem Cliente"),
+      ),
+      body: Container(
+        decoration: BoxDecoration(color: Colors.grey[200]),
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text("Imagem"),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text("Inserir email"),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text("Inserir Senha"),
+                ),
+              ],
+            ),
+          ),
         ),
-        body: Center(
-          child: Text("hello word"),
-        ));
+      ),
+    );
   }
 }
